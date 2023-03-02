01f6e039388c51239f4e36f45c431b3d510b9991
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-03-02 01:45:57 -0500

    Add files via upload

---
+++ day11.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+	
+	int a ;
+	
+	printf("Enter any Number to find it is Odd or Even : ");
+	scanf("%d",&a);
+	
+	a%2==0
+	?
+		printf("\n\ncongratulations!!\nValue you have Enter is Even.")
+	:
+		printf("\n\ncongratulations!!\nValue you have Enter is Odd.");	
+	
+	return 0 ;
+}
